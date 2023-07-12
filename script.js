65ecbbe23283c0d541cf34b9b246075769da9a8a
J.B. Dandrea <jbdandrea.skye@gmail.com>
2023-07-12 14:27:56 -0400

    Supported broken tiles causing point loss.
    I handled the changes to the input element to make sure the final score
    is affected by broken tiles. I also cached the previous scoring of the
    grid to make this process faster since there is no reason to recalculate
    the grid when only the broken tiles amount changes.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-// Creates the grid representing the tiles
+// Simple Global Variables
 const gridSize = 5;
+let lastGridScore = 0;
+
+// Creates the grid representing the tiles
 const grid = [];
 for (let i = 0; i < gridSize; i++) {
     let temp = [];
@@ -7,6 +10,72 @@ for (let i = 0; i < gridSize; i++) {
         temp.push(false);
     }
     grid.push(temp);
+}
+
+/*
+ Scores a single tile by only looking to the right and down.
+ Otherwise, tiles could be scored multiple times.
+*/
+function scoreTile(row, col) {
+    let score = 0;
+    if (grid[row][col]) {
+        score++;
+
+        for (let i = 1; i + row < gridSize && grid[i + row][col]; i++) {
+            score++;
+        }
+
+        for (let i = 1; i + col < gridSize && grid[row][i + col]; i++) {
+            score++;
+        }
+    }
+    return score;
+}
+
+// Calculates score for the entire grid;
+function calculateScore(rescoreGrid=true) {
+    let score = 0;
+
+    // Scores tiles and does bonus score for filled rows and columns
+    if (rescoreGrid) {
+        for (let i = 0; i < gridSize; i++) {
+            let rowCheck = true;
+            let colCheck = true;
+            for (let j = 0; j < gridSize; j++) {
+                score += scoreTile(i, j);
+                rowCheck = rowCheck && grid[i][j];
+                colCheck = colCheck && grid[j][i];
+            }
+            if (rowCheck) {
+                score += 2;
+            }
+            if (colCheck) {
+                score += 7;
+            }
+        }
+
+        // Bonus score for all of a color
+        for (let i = 0; i < gridSize; i++) {
+            let colorCheck = true;
+            for (let j = 0; colorCheck && j < gridSize; j++) {
+                colorCheck = grid[j][(j + i) % gridSize]; 
+            }
+            if (colorCheck) {
+                score += 10;
+            }
+        }
+        lastGridScore = score;
+    } else {
+        score = lastGridScore;
+    }
+
+    // Subtract from score for broken tiles
+    let broken = parseInt(document.querySelector('#broken').value);
+    if (broken > 0) {
+        score -= broken;
+    }
+
+    document.querySelector('#score').innerText = `Final Score: ${score}`;
 }
 
 // Sets itself up to handle tile clicks
@@ -26,52 +95,6 @@ const tileClick = tile => {
 };
 tiles.forEach(currentValue => currentValue.onclick = () => tileClick(currentValue));
 
-function scoreTile(row, col) {
-    let score = 0;
-    if (grid[row][col]) {
-        score++;
-
-        for (let i = 1; i + row < gridSize && grid[i + row][col]; i++) {
-            score++;
-        }
-
-        for (let i = 1; i + col < gridSize && grid[row][i + col]; i++) {
-            score++;
-        }
-    }
-    return score;
-}
-
-function calculateScore() {
-    let score = 0;
-
-    // Scores tiles and does bonus score for filled rows and columns
-    for (let i = 0; i < gridSize; i++) {
-        let rowCheck = true;
-        let colCheck = true;
-        for (let j = 0; j < gridSize; j++) {
-            score += scoreTile(i, j);
-            rowCheck = rowCheck && grid[i][j];
-            colCheck = colCheck && grid[j][i];
-        }
-        if (rowCheck) {
-            score += 2;
-        }
-        if (colCheck) {
-            score += 7;
-        }
-    }
-
-    // Bonus score for all of a color
-    for (let i = 0; i < gridSize; i++) {
-        let colorCheck = true;
-        for (let j = 0; colorCheck && j < gridSize; j++) {
-            colorCheck = grid[j][(j + i) % gridSize]; 
-        }
-        if (colorCheck) {
-            score += 10;
-        }
-    }
-
-    document.querySelector('h2').innerText = `Final Score: ${score}`;
-}
+// Set up to update score based on broken tiles
+const broken = document.querySelector('#broken');
+broken.oninput = () => calculateScore(false);
